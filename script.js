dce1e2bf9f1f007a592767ac3f830ecba5862da0
biplob14 <biploburrahaman@gmail.com>
2020-12-24 17:33:16 +0600

    added random options

--- script.js
+++ script.js
@@ -45,6 +45,36 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener('DOMContentLoaded', function(){
-    const items = reviews[currentItem];
+    showPerson(currentItem);
+});
+
+//show person based on item
+function showPerson(person) {
+    const items = reviews[person];
     img.src = items.img;
+    author.textContent = items.name;
+    job.textContent = items.job;
+    info.textContent = items.text;
+}
+
+// show next person 
+nextBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1)
+        currentItem = 0;
+    showPerson(currentItem);
+});
+
+// show previous person 
+prevBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0 )
+        currentItem = reviews.length - 1;
+    showPerson(currentItem);
+});
+//show random person
+randomBtn.addEventListener('click', function() {
+    currentItem = Math.floor(Math.random() * reviews.length)
+    showPerson(currentItem);
+    console.log(currentItem);
 });
